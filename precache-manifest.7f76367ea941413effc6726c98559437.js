304202a0174b12dc6f35c14a9244f94b89391515
Ashish Ranjan <ashish@evie.ai>
2019-09-04 00:57:14 +0800

    initial commit

---
+++ precache-manifest.7f76367ea941413effc6726c98559437.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd748864f7b4b9744c4d064bc6b2e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "450ed439c49e449487d4",
+    "url": "/static/css/main.e2e69099.chunk.css"
+  },
+  {
+    "revision": "a6184ade1ba174e12cda",
+    "url": "/static/js/2.a40c34f4.chunk.js"
+  },
+  {
+    "revision": "450ed439c49e449487d4",
+    "url": "/static/js/main.4ba6fd90.chunk.js"
+  },
+  {
+    "revision": "a272aa08d0b320afefd6",
+    "url": "/static/js/runtime~main.6fba1a0a.js"
+  },
+  {
+    "revision": "8a7c09b4ca096da659300f4af73e266e",
+    "url": "/static/media/clip.8a7c09b4.png"
+  },
+  {
+    "revision": "3ecd92f45add775ebff0f7799ad3bf91",
+    "url": "/static/media/string-horiz.3ecd92f4.png"
+  },
+  {
+    "revision": "6259345f0b8a1b71099a64d2fc7e5e04",
+    "url": "/static/media/string.6259345f.png"
+  }
+]);
